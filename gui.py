96f94833f7be2779f11c5e92118500643b787d55
j129008 <j129008@gmail.com>
2015-09-03 16:13:48 +0800

    增加輸入區域

--- gui.py
+++ gui.py
@@ -7,9 +7,18 @@ class fooFrame(Frame):
         self.createWidgets()
 
     def createWidgets(self):
+        # "Input:"
         self.inputText=Label(self)
         self.inputText["text"]="Input:"
         self.inputText.grid(row=0,column=0)
+
+        # "_______" (input Field)
+        self.inputField=Entry(self)
+        self.inputField["width"]=50
+        self.inputField.grid(row=0,column=1,columnspan=6)
+
+
+
 
 if __name__ == '__main__':
     winObj = Tk()
