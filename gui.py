70e09ff82be5c70a588f5dfabc0215cb23a62a80
j129008 <j129008@gmail.com>
2015-09-03 16:04:33 +0800

    弄出一個frame, 放上 "input:" 字樣

--- gui.py
+++ gui.py
@@ -1,6 +1,18 @@
 from tkinter import *
 
-winObj = Tk()
-contain = Label(winObj, text="hello world!", width="30", height="5")
-contain.pack()
-winObj.mainloop()
+class fooFrame(Frame):
+    def __init__(self, master):
+        Frame.__init__(self, master)
+        self.grid()
+        self.createWidgets()
+
+    def createWidgets(self):
+        self.inputText=Label(self)
+        self.inputText["text"]="Input:"
+        self.inputText.grid(row=0,column=0)
+
+if __name__ == '__main__':
+    winObj = Tk()
+    contain=fooFrame(master=winObj)
+    contain.pack()
+    winObj.mainloop()
